7d2a98c4d10b055cedb0d8cc33e6fa7bdaf6ed9f
Alex Prior <47067268+priorpeak@users.noreply.github.com>
2021-11-04 16:31:29 -0400

    Initial skeleton code

--- App.js
+++ App.js
@@ -1,56 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
+import { decode, encode } from "base-64";
+import { BarcodeScreen } from "./src/screens";
 
-import Amplify from 'aws-amplify';
-import awsmobile from './src/aws-exports';
-Amplify.configure(awsmobile);
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
-import Screen from './app/components/SplitScreen';
-import AppTextInput from './app/components/AppTextInput';
+const Stack = createStackNavigator();
 
-import PlaceholderScreen from './app/screens/PlaceholderScreen';
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
-import JoinIntroScreen from './app/screens/Onboarding/Intro/JoinIntroScreen';
-import SignInScreen from './app/screens/Onboarding/Authentication/SignInScreen';
-import RequestPassEmailScreen from './app/screens/Onboarding/Authentication/RequestPassEmailScreen';
-import ResendPassEmailScreen from './app/screens/Onboarding/Authentication/ResendPassEmailScreen';
-import ResetPassScreen from './app/screens/Onboarding/Authentication/ResetPassScreen';
-import ValidResetScreen from './app/screens/Onboarding/Authentication/ValidResetScreen';
-import VerifyAccountScreen from './app/screens/Onboarding/Authentication/VerifyAccountScreen';
-
-
-import IntroNavigator from './app/navigation/IntroNavigator';
-import AuthNavigator from './app/navigation/AuthNavigator';
-import { render } from 'react-dom';
-
-export default class App extends React.Component {
-  state = {
-    isAuthenticated: false
-  }
-
-  authenticate(isAuthenticated)  {
-    this.setState({ isAuthenticated });
-  }
-
-  render() {
-
-    if (this.state.isAuthenticated){
-      console.log('Auth: ', Auth)
-      return (
-        <PlaceholderScreen/>
-      )
-    }
-    return (
-       <NavigationContainer>
-        <AuthNavigator
-          screenProps={{
-            authenticate: this.authenticate.bind(this)
-          }}
-        />
-       </NavigationContainer>
-  
-    )
-  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Barcode" component={BarcodeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
