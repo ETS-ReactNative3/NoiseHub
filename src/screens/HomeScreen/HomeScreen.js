19e8c82963da8a0588189ea4f49a1430a31bb538
ibchand <ichand@bu.edu>
2021-11-17 15:12:07 -0500

    Successfully queried Timestream db

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -7,86 +7,48 @@ import styles from "./styles";
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-timestream-query/index.html#getting-started
-import { TimestreamQueryClient, CancelQueryCommand, QueryCommand, DescribeEndpointsCommand } from "@aws-sdk/client-timestream-query";
+import { TimestreamQuery, TimestreamQueryClient, CancelQueryCommand, QueryCommand, DescribeEndpointsCommand } from "@aws-sdk/client-timestream-query";
+import { TimestreamWrite, TimestreamWriteClient, CreateDatabaseCommand } from "@aws-sdk/client-timestream-write";
+
 
 import 'react-native-get-random-values';
 import "react-native-url-polyfill/auto";
 import { v4 as uuidv4 } from 'uuid';
 
+async function f0() {
+  const region = "us-east-2";
+  const endpointsQueryClient = new TimestreamQuery({ 
+    region,
+    credentials: {accessKeyId: "AKIATBZ4AXFK6BGWSUO4", secretAccessKey: "Sar5pqcUBJ11l3xIMGihnsBmMFVLtgSY9EMBla4J"},
+  });
 
-// From sample code given for JS after creating Identity Pool 
-// Initialize the Amazon Cognito credentials provider
-// AWS.config.region = 'us-east-2'; // Region
-// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-//     IdentityPoolId: 'us-east-2:470613db-8e4f-46dc-b929-4e5208ae8549',
-// });
+  const qClientResponse = await endpointsQueryClient.describeEndpoints({});
 
-const region = "us-east-2";
-// const client = new S3Client({
-//   region,
-//   credentials: fromCognitoIdentityPool({
-//     client: new CognitoIdentityClient({ region }),
-//     // Replace IDENTITY_POOL_ID with an appropriate Amazon Cognito Identity Pool ID for, such as 'us-east-1:xxxxxx-xxx-4103-9936-b52exxxxfd6'.
-//     identityPoolId: "us-east-2:470613db-8e4f-46dc-b929-4e5208ae8549",
-//   }),
-// });
+  const queryClient = new TimestreamQuery({
+    region,
+    endpoint: `https://${qClientResponse.Endpoints[0].Address}`,
+    credentials: {accessKeyId: "AKIATBZ4AXFK6BGWSUO4", secretAccessKey: "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"},
+  });
 
-// https://aws.amazon.com/blogs/mobile/integrate-the-aws-sdk-for-javascript-into-a-react-app/
-// AWS.config.update({
-//   region: awsmobile.aws_cognito_region,
-//   credentials: new AWS.CognitoIdentityCredentials({
-//     IdentityPoolId: awsmobile.aws_cognito_identity_pool_id
-//   })
-// });
+  // Credentials via the CognitoIdentityPool is not working despite the Roles being given full-access to timestream (I also tried admin privs)
+  // credentials: fromCognitoIdentityPool({
+  //   client: new CognitoIdentityClient({ region }),
+  //   // Replace IDENTITY_POOL_ID with an appropriate Amazon Cognito Identity Pool ID for, such as 'us-east-1:xxxxxx-xxx-4103-9936-b52exxxxfd6'.
+  //   identityPoolId: "us-east-2:470613db-8e4f-46dc-b929-4e5208ae8549",
+  // }),
 
-const client = new TimestreamQueryClient({ 
-  region: "us-east-2",
-  credentials: fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region }),
-    // Replace IDENTITY_POOL_ID with an appropriate Amazon Cognito Identity Pool ID for, such as 'us-east-1:xxxxxx-xxx-4103-9936-b52exxxxfd6'.
-    identityPoolId: "us-east-2:470613db-8e4f-46dc-b929-4e5208ae8549",
-  }),
-});
-
-
-// const regionalizedClient = new TimeStreamWriteClient({ region, endpoint: /**returned endpoint**/ });
-// await regionalizedClient.send(new CreateDatabaseCommand(params));
-
-// const command = new QueryCommand({QueryString: 'SELECT * FROM table'})
-const command = new QueryCommand({QueryString: "SELECT * FROM 'noisehub-timestream'.'sensordata"})
-
-async function f1() {
-  try {
-    const data = await client.send(command);
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
+  const DatabaseName = 'noisehub-timestream';
+  const TableName = 'sensordata';
+  const QueryString = `SELECT * FROM "${DatabaseName}"."${TableName}" ORDER BY time DESC LIMIT 10`;
+  // console.log(await queryClient.query({ QueryString })); // Also a valid way to query
+  const command = new QueryCommand({QueryString: QueryString})
+  const data = await queryClient.send(command);
+  console.log(data);
 }
 
-f1();
-
+f0();
 
 export default function HomeScreen({ navigation }) {
-  // var params = {
-  //   QueryString:
-  //     "SELECT * FROM 'noisehub-timestream'.'sensordata'" /* required */,
-  //   ClientToken: "",
-  //   MaxRows: "",
-  //   NextToken: "",
-  // };
-
-  // var timestreamquery = new AWS.TimestreamQuery({
-  //   credentials: creds,
-  //   region: "us-east-2",
-  // });
-
-  // timestreamquery.query(params, function (err, data) {
-  //   if (err) console.log(err, err.stack);
-  //   // an error occurred
-  //   else console.log(data); // successful response
-  // });
-
   return (
     <View style={styles.container}>
       <Text
