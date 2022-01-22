57f560b269d215ce5e30a6e4f57d0cd4b51d5d83
Ibrahim Chand <31962750+ibchand@users.noreply.github.com>
2022-01-22 15:41:26 -0500

    Added create_user()

--- src/API/userCalls.js
+++ src/API/userCalls.js
@@ -1,6 +1,5 @@
 import { getUser, listUsers } from '../graphql/queries';
 import { createUser, updateUser, deleteUser } from '../graphql/mutations';
-import { getUser, listUsers } from '../graphql/queries'
 import { API, graphqlOperation } from 'aws-amplify';
 
 // export const creatre_user = async username => {
@@ -12,7 +11,21 @@ import { API, graphqlOperation } from 'aws-amplify';
 //   }
 // }
 
-export const get_user = async username => {
+export const create_user = async (user) => {
+  try {
+    const repsonse = await API.graphql(graphqlOperation(createUser, { input: {
+      username: user.username,
+      school: userSchool,
+      noisePref: userNoise,
+      tempPref: userTemp,
+      crowdPref: userCrowd
+    }}))
+  } catch (error) {
+    console.log('Error creating user', error);
+  }
+}
+
+export const get_user = async (username) => {
   try {
     const user = await API.graphql(graphqlOperation(getUser, {username: username}));
     return user.data.getUser;
